feb73b6e09a04a7a93841514f98f263b9dd07f49
Brandon Powell <bpowell29a@gmail.com>
2017-06-19 13:51:17 -0400

    Main program to run for end user

--- src/main.cpp
+++ src/main.cpp
@@ -5,5 +5,32 @@
 #include "babysitter.h"
 
 int main(int argc, char* argv[]) {
+    struct tm starttime, endtime, bedtime;
+    std::string buffer;
+
+    do {
+        std::cout << "Enter start time in 24 hour format (17:25): ";
+        std::cin >> buffer;
+    } while (gettime(&starttime, buffer) != true);
+
+    for (;;) {
+        std::cout << "Enter bed time in 24 hour format (22:30): ";
+        std::cin >> buffer;
+        if (gettime(&bedtime, buffer) == true &&
+            std::difftime(mktime(&bedtime), mktime(&starttime)) > 0) {
+            break;
+        }
+    }
+
+    for (;;) {
+        std::cout << "Enter leave time in 24 hour format (23:30): ";
+        std::cin >> buffer;
+        if (gettime(&endtime, buffer) == true &&
+            std::difftime(mktime(&endtime), mktime(&bedtime)) > 0) {
+            break;
+        }
+    }
+
+    std::cout << calculatepay(starttime, bedtime, endtime) << std::endl;
     return 0;
 }
